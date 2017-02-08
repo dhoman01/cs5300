bcda5b2792c92d660eb29677be72b338393d1b61
Dustin Homan <dustin.e.homan@gmail.com>
2017-02-08 09:25:32 -0700

    Wed class + symbal table

---
+++ project/brains/utils/LookupTable.hpp
@@ -0,0 +1,57 @@
+#ifndef LOOKUP_TABLE_HPP
+#define LOOKUP_TABLE_HPP
+
+#include <vector>
+#include <string>
+#include <map>
+#include <exception>
+
+template<typename Info>
+class LookUpTable
+{
+public:
+    LookUpTable();
+    void store(std::string id, Info info)
+    {
+        auto f = lookupTable.back().find(id);
+        if(f != lookupTable.back().end())
+            throw std::runtime_error(id + " is already defined");
+        lookupTable.back()[id] = info;
+    }
+    Info lookup(std::string id)
+    {
+        for(int i = lookupTable.size() - 1; i >= 0; --i)
+        {
+            auto f = lookupTable[i].find(id);
+            if(f != lookupTable[i].end())
+            {
+                return f->second;
+            }
+        }
+
+        throw std::runtime_error(id + " is not defined");
+    }
+    void enterScope()
+    {
+        lookupTable.emplace_back();
+    }
+    void exitScope()
+    {
+        lookupTable.pop_back();
+    }
+    bool inLocalScope()
+    {
+        return lookupTable.size() == 3u;
+    }
+private:
+    std::vector<std::map<std::string, Info>> lookupTable;
+};
+/* Need
+struct VariableInfo
+{
+    type*;
+    location;
+}
+*/
+
+#endif
