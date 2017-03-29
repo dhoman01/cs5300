ed8c65e019ff5a8a7ee76435e03cf8b92d1c721a
Dustin Homan <dustin.e.homan@gmail.com>
2017-03-29 16:06:37 -0600

    Renamed some members

--- project/brains/utils/LookupTable.hpp
+++ project/brains/utils/LookupTable.hpp
@@ -12,6 +12,8 @@
 namespace cpsl
 {
 
+using scope = std::vector< std::map<std::string, std::shared_ptr<cpsl::Info>> >;
+
 template<typename Info>
 class LookUpTable
 {
@@ -19,17 +21,17 @@ public:
     LookUpTable(){};
     void store(std::string id, std::shared_ptr<cpsl::Info> info)
     {
-        auto f = lookupTable.back().find(id);
-        if(f != lookupTable.back().end())
+        auto f = scopes.back().find(id);
+        if(f != scopes.back().end())
             throw std::runtime_error(id + " is already defined");
-        lookupTable.back()[id] = info;
+        scopes.back()[id] = info;
     }
     std::shared_ptr<cpsl::Info> lookup(std::string id)
     {
-        for(int i = lookupTable.size() - 1; i >= 0; --i)
+        for(int i = scopes.size() - 1; i >= 0; --i)
         {
-            auto f = lookupTable[i].find(id);
-            if(f != lookupTable[i].end())
+            auto f = scopes[i].find(id);
+            if(f != scopes[i].end())
             {
                 return f->second;
             }
@@ -39,19 +41,18 @@ public:
     }
     void enterScope()
     {
-        lookupTable.emplace_back();
+        scopes.emplace_back();
     }
     void exitScope()
     {
-        lookupTable.pop_back();
+        scopes.pop_back();
     }
     bool inLocalScope()
     {
-        return lookupTable.size() == 3u;
+        return scopes.size() == 3u;
     }
 private:
-    std::vector< std::map<std::string, std::shared_ptr<cpsl::Info>> > lookupTable;
-    std::vector<int> localLocations;
+    scope scopes;
 };
 
 };
